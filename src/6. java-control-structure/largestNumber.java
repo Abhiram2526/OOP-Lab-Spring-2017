5abdddb92f07f09f5d6ebcd01a92743fa6923eb8
bksahu <bablusahoo16@gmail.com>
2017-02-23 15:25:58 +0530

    java-control-structure/largestNumber.java added

---
+++ src/6. java-control-structure/largestNumber.java
@@ -0,0 +1,25 @@
+/**
+* Write a Java application that inputs a series of 10 integers and determines and prints the largest integer.
+*/
+
+ import java.util.Scanner;
+
+ public class largestNumber {
+ 	public static void main(String[] args) {
+
+ 		Scanner input = new Scanner(System.in);
+
+ 		System.out.print("Enter Number 1: ");
+ 		int largest = input.nextInt();
+ 		
+ 		for(int i=1;i<10;i++) {
+ 			System.out.printf("Enter Number %d: ", i+1);
+ 			int number = input.nextInt();
+
+ 			if(number>largest) 
+ 				largest = number;
+ 		}
+
+ 		System.out.println("\nLargest: " + largest);
+ 	}
+ }
