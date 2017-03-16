468b8032feb2912bdf1aa360dbfc39c27f0620b7
bksahu <bablusahoo16@gmail.com>
2017-03-16 08:24:13 +0530

    pTriplets.java Updated

--- src/lab-work/6. java-control-structure/pTriplets.java
+++ src/lab-work/6. java-control-structure/pTriplets.java
@@ -6,47 +6,21 @@
 * method is an example of “brute-force” computing.
 */
 
- import java.util.Scanner;
  
  public class pTriplets {
  	public static void main(String[] args) {
  		
- 		Scanner input = new Scanner(System.in);
- 		
- 		boolean flag = false;
- 		System.out.printf("Enter first side : ");
- 		int a = input.nextInt();
- 		System.out.printf("Enter second side : ");
- 		int b = input.nextInt();
- 		System.out.printf("Enter third side : ");
- 		int c = input.nextInt();
- 
-		 
- 		// Check if sides are NON-zero		
- 		if((a>0&&b>0)&&(c>0)) 
- 		{
-			// Check if side of a triangle
- 			if(((a + b) > c&&(b + c) > a)&&(a + c) > b) 
- 			{ 
- 				// Check if right triangle
- 				if((a*a + b*b) == c*c) 
-					flag = true;
-				else if((a*a + c*c) == b*b)
-					flag = true;
-				else if((c*c + b*b) == a*a)
-					flag = true;
-				else 
-					flag = false;
+ 		for(int side1=1; side1<=500; side1++ ) {
+ 			for(int side2=1; side2<=500; side2++) {
+ 				for(int side3=1; side3<=500; side3++) {
+ 					if((side1*side1 + side2*side2)==(side3*side3))
+ 						if(side1<side2)
+ 							System.out.println(side1+" "+side2+" "+side3);
+ 				}
  			}
-		}
-		
- 		
- 		if(flag == false)
- 			System.out.println("NOT a Side of a Right triangle");
- 		else	
- 			System.out.println("Side of a Right triangle");
  			
  		}
- }
+ 	}
+}
 
  		
