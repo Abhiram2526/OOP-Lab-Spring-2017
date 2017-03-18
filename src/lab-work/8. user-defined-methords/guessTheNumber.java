b907a77c3387143d90d3cefe1d605639c6f02502
bksahu <bablusahoo16@gmail.com>
2017-03-18 07:29:18 +0530

    Bugs Fixed

--- src/lab-work/8. user-defined-methords/guessTheNumber.java
+++ src/lab-work/8. user-defined-methords/guessTheNumber.java
@@ -12,16 +12,19 @@
  public class guessTheNumber {
  	private static final SecureRandom randomNumber = new SecureRandom();
  	private static final Scanner input = new Scanner(System.in);
+ 	private static int numberOfTries = 0;
 
  	public static void main(String[] args) {
 
  		while(true) {
  		System.out.println("\n\t\t\t  GUESS THE NUMBER GAME\n");
- 		System.out.printf("\n1. PLAY\n2. EXIT\n>>");
+ 		System.out.printf("\n1.PLAY\n2.NUMBER OF TRIES\n3.EXIT\n> ");
  		int choice = input.nextInt();
 
  		if(choice == 1)
  			guessNumberGame();
+ 		else if(choice == 2)
+ 			System.out.println("Your have tried " + numberOfTries + " times");
  		else
  			break;	
  		}
@@ -30,28 +33,32 @@
 
  	// guessNumberGame() methord 
  	public static void guessNumberGame() {
- 		int generatedGuess = 1 + randomNumber.nextInt(1000);
- 		while(true) {
+		boolean hasWon = false; 		
+ 		int numberToGuess = 1 + randomNumber.nextInt(1000);
+
+ 		while(hasWon == false) {
 
  			System.out.printf("\nEnter Your Guess (Between 1-1000) : ");
  			int userGuess = input.nextInt();
 
-			if((userGuess!=generatedGuess)&&((userGuess - generatedGuess)>50)) {
+			if(userGuess > numberToGuess) {
  				System.out.println("Your guess is TOO HIGH. Try Again.");
  			}
- 			if((userGuess!=generatedGuess)&&((userGuess - generatedGuess)<50))
+ 			else if(userGuess < numberToGuess) {
  				System.out.println("Your guess is TOO LOW. Try Again.");
- 			if((userGuess - generatedGuess)==0)
+ 			}
+ 			else if(userGuess == numberToGuess)
  			{
  				System.out.println("\n\t\t\t  CONGRATULATIONS YOU WON!!!\n\n");
- 				System.out.printf("PRESS 1 to PLAY AGAIN or 2 to EXIT\n>>");
+ 				System.out.printf("PRESS 1 to PLAY AGAIN or 2 to EXIT\n> ");
  				int choice = input.nextInt();
  				if(choice==1)
  					guessNumberGame();
  				else
- 					break;
+ 					hasWon = true;
 
  			}
+ 			++numberOfTries;
  		} 
  	}
 
